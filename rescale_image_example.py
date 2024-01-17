c83b4c7c94547a9d202442188c4f8f5d9e2703be
Katherine456719 <alexandra.lung@s.unibuc.ro>
2024-01-17 22:35:11 +0200

    Refractor code

--- rescale_image_example.py
+++ rescale_image_example.py
@@ -1,5 +1,3 @@
-from skimage.color import rgb2gray
-
 import lanczos
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,12 +7,17 @@ import matplotlib
 matplotlib.use("TkAgg")
 
 
+def load_array_image(path_to_image, mode="RGB"):
+    if mode == "greyscale":
+        return np.array(Image.open(path_to_image).convert('L'))
+    return np.array(Image.open(path_to_image))
+
+
 def calculate_fft(image):
     return 20 * np.log10(np.abs(np.fft.fftshift(np.fft.fft2(image))))
 
 
-def lanczos_rgb_image_example(image, new_height, new_width):
-
+def rescale_rgb_image(image, new_height, new_width):
     # Get the dimension and channels of original images
     old_height = image.shape[0]
     old_width = image.shape[1]
@@ -28,19 +31,18 @@ def lanczos_rgb_image_example(image, new_height, new_width):
 
         # Apply Lanczos interpolation to each channel
         upscaled_channel = lanczos.interpolate_lanczos2_fast(
-            original_array[:, :, channel], at_x, at_y, 2
+            image[:, :, channel], at_x, at_y, 2
         )
         upscaled_channel = np.clip(upscaled_channel, 0, 255)
         upscaled_channels.append(upscaled_channel)
 
     # Stack the upscaled channels
-    upscaled = np.stack(upscaled_channels, axis=-1)
-    upscaled = upscaled.astype(np.uint8)
+    upscaled_image = (np.stack(upscaled_channels, axis=-1)).astype(np.uint8)
 
-    return upscaled
+    return upscaled_image
 
 
-def lanczos_grayscale_image_example(image, new_height, new_width):
+def rescale_greyscale_image(image, new_height, new_width):
     # Get the dimension and channels of original images
     old_height = image.shape[0]
     old_width = image.shape[1]
@@ -50,36 +52,52 @@ def lanczos_grayscale_image_example(image, new_height, new_width):
     at_x, at_y = np.meshgrid(at_x, at_y)
 
     # Apply Lanczos interpolation to each channel
-    upscaled = lanczos.interpolate_lanczos2(original_array, at_x, at_y, 2)
-    upscaled = np.clip(upscaled, 0, 255)
-    upscaled = upscaled.astype(np.uint8)
+    upscaled_image = lanczos.interpolate_lanczos2_fast(image, at_x, at_y, 2)
+    upscaled_image = np.clip(upscaled_image, 0, 255)
+    upscaled_image = upscaled_image.astype(np.uint8)
 
-    return upscaled
+    return upscaled_image
 
 
 if __name__ == "__main__":
-    # lanczos_rgb_image_example(original_array, new_height, new_width)
-
-    # Load the image
     input_image_path = 'bunny.png'
-    original_image = Image.open(input_image_path).convert('L')
-    # Convert the image to a NumPy array
-    original_array = np.array(original_image)
-    original_height = original_array.shape[0]
-    original_width = original_array.shape[1]
-    fft_original = calculate_fft(original_array)
-    new_width = 60
-    new_height = 30
-    upscaled = lanczos_grayscale_image_example(original_array, new_height, new_width)
+    image = load_array_image(input_image_path)
+
+    new_width = 800
+    new_height = 300
+
+    # Rgb example
+    rgb_rescaled = rescale_rgb_image(image, new_height, new_width)
+
+    fig, axs = plt.subplots(2, 1)
+    axs[0].set_title(f"{image.shape[0]}x{image.shape[1]}")
+    axs[0].imshow(image, cmap=plt.get_cmap('gray'))
+
+    axs[1].set_title(f"{new_height}x{new_width}")
+    axs[1].imshow(rgb_rescaled, cmap=plt.get_cmap('gray'))
+
+    fig.tight_layout()
+    plt.show()
+
+    # Greyscale example
+    image = load_array_image(input_image_path, "greyscale")
+    fft_original = calculate_fft(image)
+
+    upscaled = rescale_greyscale_image(image, new_height, new_width)
     fft_upscale = calculate_fft(upscaled)
 
     fig, axs = plt.subplots(2, 2)
-    axs[0, 0].set_title(f"{original_height}x{original_width}")
-    axs[0, 0].imshow(original_array, cmap=plt.get_cmap('gray'))
-    axs[0, 0].imshow(original_array, cmap=plt.get_cmap('gray'))
+    axs[0, 0].set_title(f"{image.shape[0]}x{image.shape[1]}")
+    axs[0, 0].imshow(image, cmap=plt.get_cmap('gray'))
+
+    axs[0, 1].set_title(f"FFT 2D")
     axs[0, 1].imshow(fft_original)
+
     axs[1, 0].set_title(f"{new_height}x{new_width}")
     axs[1, 0].imshow(upscaled, cmap=plt.get_cmap('gray'))
+
+    axs[1, 1].set_title(f"FFT 2D")
     axs[1, 1].imshow(fft_upscale)
+
     fig.tight_layout()
     plt.show()
